3ec11bc2dc2c089e07bbe28e42756804b154f1ec
Jeferson Vargas <dmaistecnologia@gmail.com>
2018-08-06 19:49:06 -0300

    implementado sistema de verificação em PHP

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 <html>
 <head>
     <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Page Title</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="estilo.css" />
@@ -10,18 +9,35 @@
 </head>
 <body>
 <?php
-    if (isset($_POST['nome']))
-    {
-        echo "enviado";
-    }
-    else {
-        echo "nao enviado";
-    }
-?>
-<form method="POST">
-    <input type="text" name="nome" placeholder="Nome..." />
-    <input type="submit" valor="Enviar" />
-</form>
+		if (empty($_POST['email']) == false) 
+		{
+			if (empty($_POST['senha']) == false) 
+			{
+
+				$email = $_POST['email'];
+				$senha = $_POST['senha'];
+
+				echo "bem vindo: ".$email;
+				echo " sua senha é: ".$senha;
+
+			} 
+			else
+			{
+				echo "Informe sua senha.";
+			}
+		}
+		else
+		{
+			echo "Informe seu email.";
+		}
+
+	?>
+	<hr/>
+	<form class="formulario" method="POST">
+		<input type="text" name="email" placeholder="email..."></br></br>
+		<input type="password" name="senha" placeholder="senha..."></br></br>
+		<input type="submit" value="Enviar Dados">
+	</form>
     
 </body>
 </html>
