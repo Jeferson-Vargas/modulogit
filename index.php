bf9214013f6977306f7d0c1539cbdc6a8da5d2fc
Jeferson Vargas <dmaistecnologia@gmail.com>
2018-08-07 19:28:45 -0300

    mudanças dia 7

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <title>Page Title</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="estilo.css" />
-    <!-- <script src="main.js"></script> -->
+    <script rel="text/javascript" src="script.js"></script>
 </head>
 <body>
 <?php
@@ -29,18 +29,68 @@
 		{
 			echo "Informe seu email.";
 		}
-
+		//header("Location: index.php");
 	?>
 	<hr/>
-	<form class="formulario" method="POST">
-		<input type="text" name="email" placeholder="email..."></br></br>
-		<input type="password" name="senha" placeholder="senha..."></br></br>
-		<input type="submit" value="Enviar Dados">
-	</form>
-	<div class="teste">
-		<form class="formulario" method="POST">
-		<input type="text" name="texto" />
-		</form>	
+	<div class="container">
+		<div class="formulario">
+			<form method="POST">
+				<input type="text" name="email" placeholder="email..." required="email"></br></br>
+				<input type="password" name="senha" placeholder="senha..." required="senha"></br></br>
+				<input type="submit" value="Enviar Dados">
+			</form>
+		</div>
+	</div>
+	<div class="area">
+		<div class="botao">
+			<button onclick="atualizar()">Atualizar</button>
+		</div>
+	</div>
+
+	<hr/>
+	<h1>Atribuir valor via javascript</h1>
+	<input type="text" id="teste"></br></br>
+	<button id="Inserir" onclick="atribuir()">Inserir</button>
+	<hr/>
+
+	<h1>Alterar lista pelo javascript!</h1>
+
+	<input type="text" id="ingrediente" />
+
+	<button onclick="adicionarIngrediente()">Adicionar</button></body></html></br>
+
+	<ul id="lista">
+		<li>Item 1</li>
+		<li>Item 2</li>
+		<li>Item 3</li>
+	</ul>
+	<hr/>
+	<div class="areaCalc">
+		<div class="calculadora">
+			<fieldset>
+           		<legend>Calculadora Simples</legend>
+				<h2>Escolha o tipo de Operação: </h2>
+              	<select id="option">
+                	<option value="1" selected="selected"> Somar </option >
+                	<option value="2"> Subtrair </option >
+                	<option value="3"> Dividir </option >
+                	<option value="4"> Multiplicar </option >
+               	</select>
+               	<br/><br/>
+               	Numero 1 :
+               	<input type="text" name="numero" id="numero1"><br/><br/>
+
+               	Numero 2 :
+                <input type="text" name="numero" id="numero2"><br/><br/>
+
+                 Total: <input type="text" id="somaTotal" class="formtTotal" disabled /> 
+
+                <button type="button" onclick="calculadora()">enviar</button>
+                <button type="reset" onclick="limp()">Limpar</button><br/><br/>
+              
+
+			</fieldset>
+		</div>
 	</div>
 </body>
 </html>
--- script.js
+++ script.js
@@ -10,4 +10,67 @@ var y = 20;
 /*
 	Escrever na pagina
 */
-document.write("<h1> Teste de JavaScript</h1>");
+document.write("<h1> Teste de JavaScript</h1><hr/>");
+
+/*
+	Ações
+
+*/
+
+function atualizar(){
+	window.location.href = window.location.href;
+}
+function atribuir()
+{
+	var alvo = document.getElementById('teste');
+	var valor = prompt("Qual seu nome?");
+	alvo.value = valor;
+}
+
+/*
+	Mudar li pelo javascript
+*/
+function adicionarIngrediente(){
+	var ing = document.getElementById("ingrediente").value;
+	var listaAtual = document.getElementById("lista").innerHTML;
+	listaAtual = listaAtual+"<li>"+ing+"</li>";
+	document.getElementById("lista").innerHTML = listaAtual;
+
+	//limpar campo
+	document.getElementById("ingrediente").value = "";
+}
+
+/*
+	Calculadora
+*/
+function calculadora(){
+    
+    var numero1 = document.getElementById("numero1").value; 
+    var numero2 = document.getElementById("numero2").value;
+    var valor = parseInt(document.getElementById("option").value);
+    
+    switch(valor){
+     
+       case 1: 
+          var result = parseInt(numero1) + parseInt(numero2); 
+      break;
+      
+       case 2: 
+          var result = parseInt(numero1) - parseInt(numero2); 
+       break;
+       
+       case 3:   
+         var result = parseInt(numero1) / parseInt(numero2); 
+       break;
+       
+       case 4:
+         var result = parseInt(numero1) * parseInt(numero2);
+         break;
+  }
+  
+     document.getElementById("somaTotal").value = result; 
+   
+ }
+function limp(){
+	document.getElementById("somaTotal").value = "";
+}
